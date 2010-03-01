cb7281ba6865254a4e3fe62c22d85434f5145194
sangwook <sangwook>
2010-03-01 16:13:52 +0000

    print APIs for WinView & WglView are updated.

--- src/winview/WinViewPrintApi.cpp
+++ src/winview/WinViewPrintApi.cpp
@@ -13,22 +13,23 @@
 #endif
 
 
+#if defined(min)
+#undef min
+#endif
+
+
 namespace swl {
 
 bool printWinViewUsingGdi(WinViewBase &view, HDC hPrintDC)
 {
-	//
-	RECT rctPage;
-	rctPage.left = 0;
-	rctPage.top = 0;
-	rctPage.right = GetDeviceCaps(hPrintDC, HORZRES);
-	rctPage.bottom = GetDeviceCaps(hPrintDC, VERTRES);
-
+	const boost::shared_ptr<WinViewBase::camera_type> &currCamera = view.topCamera();
+	if (currCamera.get())
 	{
-		const boost::shared_ptr<WinViewBase::camera_type> &currCamera = view.topCamera();
- 		if (currCamera.get())
+#if 0
+		const Region2<int> rctPage(swl::Point2<int>(0, 0), swl::Point2<int>(GetDeviceCaps(hPrintDC, HORZRES), GetDeviceCaps(hPrintDC, VERTRES)));
+
 		{
-			const std::auto_ptr<WinViewBase::context_type> printContext(new GdiPrintContext(hPrintDC, rctPage));
+			const std::auto_ptr<WinViewBase::context_type> printContext(new GdiPrintContext(hPrintDC, rctPage));  // error: use rctPage
 			const std::auto_ptr<WinViewBase::camera_type> printCamera(currCamera->cloneCamera());
 			if (printCamera.get() && printContext.get() && printContext->isActivated())
 			{
@@ -38,9 +39,35 @@ bool printWinViewUsingGdi(WinViewBase &view, HDC hPrintDC)
 				view.renderScene(*printContext, *printCamera);
 			}
 		}
-	}
+#else
+		const double eps = 1.0e-20;
 
-	return true;
+		const Region2<int> rctPage(swl::Point2<int>(0, 0), swl::Point2<int>(GetDeviceCaps(hPrintDC, HORZRES), GetDeviceCaps(hPrintDC, VERTRES)));
+		const Region2<double> &currViewRegion = currCamera->getCurrentViewRegion();
+		const double width = currViewRegion.getWidth() >= eps ? currViewRegion.getWidth() : 1.0;
+		const double height = currViewRegion.getHeight() >= eps ? currViewRegion.getHeight() : 1.0;
+		const double ratio = std::min(rctPage.getWidth() / width, rctPage.getHeight() / height);
+
+		const double width0 = width * ratio, height0 = height * ratio;
+		const int w0 = (int)std::floor(width0), h0 = (int)std::floor(height0);
+		const int x0 = rctPage.left + (int)std::floor((rctPage.getWidth() - width0) * 0.5), y0 = rctPage.bottom + (int)std::floor((rctPage.getHeight() - height0) * 0.5);
+
+		{
+			const std::auto_ptr<WinViewBase::context_type> printContext(new GdiPrintContext(hPrintDC, Region2<int>(x0, y0, x0 + w0, y0 + h0)));
+			const std::auto_ptr<WinViewBase::camera_type> printCamera(currCamera->cloneCamera());
+			if (printCamera.get() && printContext.get() && printContext->isActivated())
+			{
+				view.initializeView();
+				printCamera->setViewRegion(currCamera->getCurrentViewRegion());
+				printCamera->setViewport(0, 0, w0, h0);
+				view.renderScene(*printContext, *printCamera);
+			}
+		}
+#endif
+
+		return true;
+	}
+	else return true;
 }
 
 }  // namespace swl
