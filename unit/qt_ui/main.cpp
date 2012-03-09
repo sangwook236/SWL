b3de264b2dfd6bc92d2d257e7f6e88441fc7036e
sangwook <sangwook>
2012-03-09 07:13:50 +0000

    a project for CppUnit's QT UI is added.

---
+++ unit/qt_ui/main.cpp
@@ -0,0 +1,39 @@
+#include <qapplication.h>
+#include <cppunit/ui/qt/TestRunner.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+
+
+#if defined(UNICODE) || defined(_UNICODE)
+int wmain(int argc, wchar_t **argv)
+#else
+int main(int argc, char **argv)
+#endif
+{
+	try
+	{
+		QApplication app(argc, argv);
+
+		//CppUnit::QtUi::TestRunner runner;
+		CppUnit::QtTestRunner runner; 
+		
+		runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
+
+		//runner.setOutputter();
+		runner.run(true);
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "exception occurred !!!: " << e.what() << std::endl;
+	}
+	catch (...)
+	{
+		std::wcout << L"unknown exception occurred !!!" << std::endl;
+	}
+
+	std::wcout << L"press any key to exit ..." << std::endl;
+	std::wcout.flush();
+	std::wcin.get();
+
+    return 0;
+}
+
