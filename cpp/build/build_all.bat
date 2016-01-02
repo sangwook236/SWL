75fa15a06c7d88181a1111111d7507add9e9ee05
Sang-Wook Lee <sangwook236@gmail.com>
2016-01-01 16:40:56 -0800

    Update CMakeLists.txt.

--- cpp/build/build_all.bat
+++ cpp/build/build_all.bat
@@ -2,6 +2,9 @@
 setlocal
 
 cmake ..
+rem cmake -DCMAKE_BUILD_TYPE=Release ..
+rem cmake -DCMAKE_BUILD_TYPE=Debug ..
+
 make
 make test
 make doc
--- cpp/build/build_all.sh
+++ cpp/build/build_all.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 cmake ..
+#cmake -DCMAKE_BUILD_TYPE=Release ..
+#cmake -DCMAKE_BUILD_TYPE=Debug ..
+
 make
 make test
 make doc
