d4fe1d1e136556d0def1601a421d846ed5cb3113
Sang-Wook Lee <sangwook236@gmail.com>
2016-12-13 02:23:07 +0900

    Convert the logging system from log4cxx to Boost.Log.

--- cpp/inc/swl/base/Log.h
+++ cpp/inc/swl/base/Log.h
@@ -2,49 +2,72 @@
 #define __SWL_BASE__LOG__H_ 1
 
 
+#include "swl/base/ExportBase.h"
+#include <string>
+#include <climits>
+
+
 //#define __SWL__DISABLE_LOG_ 1
 
 #if !defined(__SWL__DISABLE_LOG_)
 
-#include <log4cxx/logger.h>
-#include <log4cxx/level.h>
-#include <log4cxx/basicconfigurator.h>
-#include <log4cxx/propertyconfigurator.h>
-#include <log4cxx/xml/domconfigurator.h>
+namespace swl {
+
+//-----------------------------------------------------------------------------------
+// Log.
+
+struct SWL_BASE_API Log
+{
+public:
+	enum LogSeverityLevel
+	{
+		OFF = INT_MAX,
+		FATAL = 50000,
+		ERROR = 40000,
+		WARN = 30000,
+		INFO = 20000,
+		DEBUG = 10000,
+		TRACE = 5000,
+		ALL = INT_MIN
+	};
+
+public:
+	static void log(const int level, const std::wstring &message);
+
+private:
+	class Initializer
+	{
+	public:
+		Initializer();
+	};
+
+private:
+	static Initializer initializer_;
+};
+
+}  // namespace swl
+
+#define SWL_LOG(level, message) swl::Log::log(level, message)
+
+#define SWL_LOG_ALL(message) swl::Log::log(Log::ALL, message)
+#define SWL_LOG_TRACE(message) swl::Log::log(Log::TRACE, message)
+#define SWL_LOG_DEBUG(message) swl::Log::log(Log::DEBUG, message)
+#define SWL_LOG_INFO(message) swl::Log::log(Log::INFO, message)
+#define SWL_LOG_WARN(message) swl::Log::log(Log::WARN, message)
+#define SWL_LOG_ERROR(message) swl::Log::log(Log::ERROR, message)
+#define SWL_LOG_FATAL(message) swl::Log::log(Log::FATAL, message)
 
 #else
 
-#undef LOG4CXX_LOG
-#undef LOG4CXX_LOGLS
+#define SWL_LOG(level, message)
 
-#undef LOG4CXX_TRACE
-#undef LOG4CXX_DEBUG
-#undef LOG4CXX_INFO
-#undef LOG4CXX_WARN
-#undef LOG4CXX_ERROR
-#undef LOG4CXX_ASSERT
-#undef LOG4CXX_FATAL
-
-#undef LOG4CXX_L7DLOG
-#undef LOG4CXX_L7DLOG1
-#undef LOG4CXX_L7DLOG2
-#undef LOG4CXX_L7DLOG3
-
-#define LOG4CXX_LOG(logger, level, message)
-#define LOG4CXX_LOGLS(logger, level, message)
-
-#define LOG4CXX_DEBUG(logger, message)
-#define LOG4CXX_TRACE(logger, message)
-#define LOG4CXX_INFO(logger, message)
-#define LOG4CXX_WARN(logger, message)
-#define LOG4CXX_ERROR(logger, message)
-#define LOG4CXX_ASSERT(logger, condition, message)
-#define LOG4CXX_FATAL(logger, message)
-
-#define LOG4CXX_L7DLOG(logger, level, key)
-#define LOG4CXX_L7DLOG1(logger, level, key, p1)
-#define LOG4CXX_L7DLOG2(logger, level, key, p1, p2)
-#define LOG4CXX_L7DLOG3(logger, level, key, p1, p2, p3)
+#define SWL_LOG_ALL(message)
+#define SWL_LOG_TRACE(message)
+#define SWL_LOG_DEBUG(message)
+#define SWL_LOG_INFO(message)
+#define SWL_LOG_WARN(message)
+#define SWL_LOG_ERROR(message)
+#define SWL_LOG_FATAL(message)
 
 #endif
 
--- cpp/unit/testcase/base/LogTest.cpp
+++ cpp/unit/testcase/base/LogTest.cpp
@@ -1,8 +1,8 @@
 #include "swl/Config.h"
 #include "../../UnitTestConfig.h"
 
-//#define __SWL__DISABLE_LOGGER_ 1
-#include "swl/base/Logger.h"
+//#define __SWL__DISABLE_LOG_ 1
+#include "swl/base/Log.h"
 
 
 #if defined(_DEBUG) && defined(__SWL_CONFIG__USE_DEBUG_NEW)
@@ -21,7 +21,7 @@ namespace unit_test {
 
 namespace {
 
-struct LoggerTest
+struct LogTest
 {
 private:
 	struct Fixture
@@ -40,49 +40,29 @@ public:
 	{
 		Fixture fixture;
 
-		LOG4CXX_WARN(logger_, L"Low fuel level.");
-		LOG4CXX_ERROR(tracer_, L"Located nearest gas station.");
+		SWL_LOG_WARN(L"Low fuel level.");
+		SWL_LOG_ERROR(L"Located nearest gas station.");
 
-		logger_->setLevel(log4cxx::Level::getInfo());
-		BOOST_CHECK_EQUAL(log4cxx::Level::getInfo(), logger_->getLevel());
-
-		LOG4CXX_DEBUG(logger_, L"Starting search for nearest gas station.");
-		LOG4CXX_DEBUG(tracer_, L"Exiting gas station search.");
+		SWL_LOG_DEBUG(L"Starting search for nearest gas station.");
+		SWL_LOG_DEBUG(L"Exiting gas station search.");
 	}
 
 public:
-	LoggerTest()
-	: logger_(log4cxx::Logger::getLogger(L"swlLogger.logger")), tracer_(log4cxx::Logger::getLogger(L"swlLogger.tracer"))
+	LogTest()
 	{
-		const int config = 1;
-		switch (config)
-		{
-		case 1:
-			log4cxx::PropertyConfigurator::configure(L"./data/test_data/swl_logger_conf.properties");
-			break;
-		case 2:
-			log4cxx::xml::DOMConfigurator::configure(L"./data/test_data/swl_logger_conf.xml");
-			break;
-		case 0:
-		default:
-			log4cxx::BasicConfigurator::configure();
-			break;
-		}
 	}
 
 private:
-	log4cxx::LoggerPtr logger_;
-	log4cxx::LoggerPtr tracer_;
 };
 
-struct LoggerTestSuite: public boost::unit_test_framework::test_suite
+struct LogTestSuite: public boost::unit_test_framework::test_suite
 {
-	LoggerTestSuite()
-	: boost::unit_test_framework::test_suite("SWL.Base.Logger")
+	LogTestSuite()
+	: boost::unit_test_framework::test_suite("SWL.Base.Log")
 	{
-		boost::shared_ptr<LoggerTest> test(new LoggerTest());
+		boost::shared_ptr<LogTest> test(new LogTest());
 
-		add(BOOST_CLASS_TEST_CASE(&LoggerTest::testBasic, test), 0);
+		add(BOOST_CLASS_TEST_CASE(&LogTest::testBasic, test), 0);
 
 		boost::unit_test::framework::master_test_suite().add(this);
 	}
@@ -95,31 +75,12 @@ struct LoggerTestSuite: public boost::unit_test_framework::test_suite
 
 #elif defined(__SWL_UNIT_TEST__USE_GOOGLE_TEST)
 
-class LoggerTest : public testing::Test
+class LogTest : public testing::Test
 {
 public:
-	LoggerTest()
-		: logger_(log4cxx::Logger::getLogger(L"swlLogger.logger")), tracer_(log4cxx::Logger::getLogger(L"swlLogger.tracer"))
+	LogTest()
 	{
-		const int config = 1;
-		switch (config)
-		{
-		case 1:
-			log4cxx::PropertyConfigurator::configure(L"./data/test_data/swl_logger_conf.properties");
-			break;
-		case 2:
-			log4cxx::xml::DOMConfigurator::configure(L"./data/test_data/swl_logger_conf.xml");
-			break;
-		case 0:
-		default:
-			log4cxx::BasicConfigurator::configure();
-			break;
-		}
 	}
-
-public:
-	log4cxx::LoggerPtr getLogger()  { return logger_; }
-	log4cxx::LoggerPtr getTracer()  { return tracer_; }
 
 protected:
 	/*virtual*/ void SetUp()
@@ -129,22 +90,15 @@ protected:
 	/*virtual*/ void TearDown()
 	{
 	}
-
-private:
-	log4cxx::LoggerPtr logger_;
-	log4cxx::LoggerPtr tracer_;
 };
 
-TEST_F(LoggerTest, testBasic)
+TEST_F(LogTest, testBasic)
 {
-	LOG4CXX_WARN(getLogger(), L"Low fuel level.");
-	LOG4CXX_ERROR(getTracer(), L"Located nearest gas station.");
+	SWL_LOG_WARN(L"Low fuel level.");
+	SWL_LOG_ERROR(L"Located nearest gas station.");
 
-	getLogger()->setLevel(log4cxx::Level::getInfo());
-	EXPECT_EQ(log4cxx::Level::getInfo(), getLogger()->getLevel());
-
-	LOG4CXX_DEBUG(getLogger(), L"Starting search for nearest gas station.");
-	LOG4CXX_DEBUG(getTracer(), L"Exiting gas station search.");
+	SWL_LOG_DEBUG(L"Starting search for nearest gas station.");
+	SWL_LOG_DEBUG(L"Exiting gas station search.");
 }
 
 //-----------------------------------------------------------------------------
@@ -152,10 +106,10 @@ TEST_F(LoggerTest, testBasic)
 
 #elif defined(__SWL_UNIT_TEST__USE_CPP_UNIT)
 
-struct LoggerTest: public CppUnit::TestFixture
+struct LogTest: public CppUnit::TestFixture
 {
 private:
-	CPPUNIT_TEST_SUITE(LoggerTest);
+	CPPUNIT_TEST_SUITE(LogTest);
 	CPPUNIT_TEST(testBasic);
 	CPPUNIT_TEST_SUITE_END();
 
@@ -170,39 +124,17 @@ public:
 
 	void testBasic()
 	{
-		LOG4CXX_WARN(logger_, L"Low fuel level.");
-		LOG4CXX_ERROR(tracer_, L"Located nearest gas station.");
+		SWL_LOG_WARN(L"Low fuel level.");
+		SWL_LOG_ERROR(L"Located nearest gas station.");
 
-		logger_->setLevel(log4cxx::Level::getInfo());
-		CPPUNIT_ASSERT_EQUAL(log4cxx::Level::getInfo(), logger_->getLevel());
-
-		LOG4CXX_DEBUG(logger_, L"Starting search for nearest gas station.");
-		LOG4CXX_DEBUG(tracer_, L"Exiting gas station search.");
+		SWL_LOG_DEBUG(L"Starting search for nearest gas station.");
+		SWL_LOG_DEBUG(L"Exiting gas station search.");
 	}
 
 public:
-	LoggerTest()
-	: logger_(log4cxx::Logger::getLogger(L"swlLogger.logger")), tracer_(log4cxx::Logger::getLogger(L"swlLogger.tracer"))
+	LogTest()
 	{
-		const int config = 1;
-		switch (config)
-		{
-		case 1:
-			log4cxx::PropertyConfigurator::configure(L"./data/test_data/swl_logger_conf.properties");
-			break;
-		case 2:
-			log4cxx::xml::DOMConfigurator::configure(L"./data/test_data/swl_logger_conf.xml");
-			break;
-		case 0:
-		default:
-			log4cxx::BasicConfigurator::configure();
-			break;
-		}
 	}
-
-private:
-	log4cxx::LoggerPtr logger_;
-	log4cxx::LoggerPtr tracer_;
 };
 
 #endif
@@ -211,7 +143,7 @@ private:
 }  // namespace swl
 
 #if defined(__SWL_UNIT_TEST__USE_CPP_UNIT)
-//CPPUNIT_TEST_SUITE_REGISTRATION(swl::unit_test::LoggerTest);
+//CPPUNIT_TEST_SUITE_REGISTRATION(swl::unit_test::LogTest);
 CPPUNIT_REGISTRY_ADD_TO_DEFAULT("SWL.Base");
-CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(swl::unit_test::LoggerTest, "SWL.Base");
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(swl::unit_test::LogTest, "SWL.Base");
 #endif
