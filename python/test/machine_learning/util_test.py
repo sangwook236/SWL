74be56fd2c023514a5a976d769e4a2ef350bd97b
Sang-Wook Lee <sangwook236@gmail.com>
2017-08-01 10:05:02 +0900

    Added learning rate scheduler.

--- python/test/machine_learning/util_test.py
+++ python/test/machine_learning/util_test.py
@@ -1,12 +1,11 @@
-import os
+import os, sys
 if 'posix' == os.name:
 	swl_python_home_dir_path = '/home/sangwook/work/SWL_github/python'
 else:
 	swl_python_home_dir_path = 'D:/work/SWL_github/python'
-os.chdir(swl_python_home_dir_path + '/test/machine_learning')
+sys.path.append(swl_python_home_dir_path + '/src')
 
-import sys
-sys.path.append('../../src')
+#os.chdir(swl_python_home_dir_path + '/test/machine_learning')
 
 #%%------------------------------------------------------------------
 
@@ -42,3 +41,34 @@ print(swl.machine_learning.util.to_one_hot_encoding(Y, 10))
 print('keras.utils.to_categorical ->', keras.utils.to_categorical(Y, 10).shape)
 #print(keras.utils.to_categorical(Y, 10))  // Not correctly working.
 print(keras.utils.to_categorical(Y, 10).reshape(Y.shape[:-1] + (-1,)))
+
+#%%------------------------------------------------------------------
+# time_based_learning_rate().
+
+import matplotlib.pyplot as plt
+
+initial_learning_rate = 0.1
+decay_rate = 0.001
+
+epochs = range(10000)
+learning_rates = []
+for epoch in epochs:
+	learning_rates.append(swl.machine_learning.util.time_based_learning_rate(epoch, initial_learning_rate, decay_rate))
+
+plt.figure()
+plt.plot(epochs, learning_rates)
+
+#%%------------------------------------------------------------------
+# drop_based_learning_rate().
+
+initial_learning_rate = 0.001
+drop_rate = 0.5
+epoch_drop = 10000.0
+
+epochs = range(100000)
+learning_rates = []
+for epoch in epochs:
+	learning_rates.append(swl.machine_learning.util.drop_based_learning_rate(epoch, initial_learning_rate, drop_rate, epoch_drop))
+
+plt.figure()
+plt.plot(epochs, learning_rates)
