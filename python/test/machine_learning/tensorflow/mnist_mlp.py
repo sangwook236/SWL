3ff79eaede71ff3e08be52dd3773de31fe70063c
Sang-Wook Lee <sangwook236@gmail.com>
2018-03-14 15:22:34 +0900

    Added MLP models and a runner for MNIST.

---
+++ python/test/machine_learning/tensorflow/mnist_mlp.py
@@ -0,0 +1,133 @@
+import tensorflow as tf
+import math
+from simple_neural_net import SimpleNeuralNet
+
+#%%------------------------------------------------------------------
+
+class MnistMLP(SimpleNeuralNet):
+	def __init__(self, input_shape, output_shape, model_type, max_neuron_count):
+		super().__init__(input_shape, output_shape)
+
+		self._model_type = model_type
+		self._max_neuron_count = max_neuron_count
+
+	def _create_single_model(self, input_tensor, is_training, input_shape, output_shape):
+		num_classes = output_shape[-1]
+		with tf.variable_scope('mnist_mlp', reuse=tf.AUTO_REUSE):
+			if 1 == self._model_type:  # A wide and shallow MLP with a single hidden layer.
+				return self._create_wide_and_shallow_model(input_tensor, is_training, num_classes, self._max_neuron_count)
+			elif 2 == self._model_type:  # A narrow and deep MLP with three hidden layers of triangular shape.
+				return self._create_narrow_and_deep_model_with_triangular_shape(input_tensor, is_training, num_classes, self._max_neuron_count)
+			elif 3 == self._model_type:  # A narrow and deep MLP with three hidden layers of diamond shape.
+				return self._create_narrow_and_deep_model_with_diamond_shape(input_tensor, is_training, num_classes, self._max_neuron_count)
+			elif 4 == self._model_type:  # A narrow and deep MLP with three hidden layers of hourglass shape.
+				return self._create_narrow_and_deep_model_with_hourglass_shape(input_tensor, is_training, num_classes, self._max_neuron_count)
+			else:
+				assert False, 'Invalid model type.'
+				return None
+
+	def _create_wide_and_shallow_model(self, input_tensor, is_training, num_classes, max_neuron_count):
+		with tf.variable_scope('fc1', reuse=tf.AUTO_REUSE):
+			fc1 = tf.layers.flatten(input_tensor, name='flatten')
+
+			fc1 = tf.layers.dense(fc1, max_neuron_count, activation=tf.nn.relu, name='fc')
+			#fc1 = tf.layers.dropout(fc1, rate=0.25, training=is_training, name='dropout')
+
+		with tf.variable_scope('fc2', reuse=tf.AUTO_REUSE):
+			if 1 == num_classes:
+				fc2 = tf.layers.dense(fc1, 1, activation=tf.sigmoid, name='fc')
+				#fc2 = tf.layers.dense(fc1, 1, activation=tf.sigmoid, activity_regularizer=tf.contrib.layers.l2_regularizer(0.0001), name='fc')
+			elif num_classes >= 2:
+				fc2 = tf.layers.dense(fc1, num_classes, activation=tf.nn.softmax, name='fc')
+				#fc2 = tf.layers.dense(fc1, num_classes, activation=tf.nn.softmax, activity_regularizer=tf.contrib.layers.l2_regularizer(0.0001), name='fc')
+			else:
+				assert num_classes > 0, 'Invalid number of classes.'
+
+			return fc2
+
+	def _create_narrow_and_deep_model_with_triangular_shape(self, input_tensor, is_training, num_classes, max_neuron_count):
+		num_neurons = math.ceil((max_neuron_count / 8.0) ** (1.0 / 3.0))
+
+		with tf.variable_scope('fc1', reuse=tf.AUTO_REUSE):
+			fc1 = tf.layers.flatten(input_tensor, name='flatten')
+
+			fc1 = tf.layers.dense(fc1, num_neurons, activation=tf.nn.relu, name='fc')
+			#fc1 = tf.layers.dropout(fc1, rate=0.25, training=is_training, name='dropout')
+
+		with tf.variable_scope('fc2', reuse=tf.AUTO_REUSE):
+			fc2 = tf.layers.dense(fc1, num_neurons * 2, activation=tf.nn.relu, name='fc')
+			#fc2 = tf.layers.dropout(fc2, rate=0.25, training=is_training, name='dropout')
+
+		with tf.variable_scope('fc3', reuse=tf.AUTO_REUSE):
+			fc3 = tf.layers.dense(fc2, num_neurons * 4, activation=tf.nn.relu, name='fc')
+			#fc3 = tf.layers.dropout(fc3, rate=0.25, training=is_training, name='dropout')
+
+		with tf.variable_scope('fc4', reuse=tf.AUTO_REUSE):
+			if 1 == num_classes:
+				fc4 = tf.layers.dense(fc3, 1, activation=tf.sigmoid, name='fc')
+				#fc4 = tf.layers.dense(fc3, 1, activation=tf.sigmoid, activity_regularizer=tf.contrib.layers.l2_regularizer(0.0001), name='fc')
+			elif num_classes >= 2:
+				fc4 = tf.layers.dense(fc3, num_classes, activation=tf.nn.softmax, name='fc')
+				#fc4 = tf.layers.dense(fc3, num_classes, activation=tf.nn.softmax, activity_regularizer=tf.contrib.layers.l2_regularizer(0.0001), name='fc')
+			else:
+				assert num_classes > 0, 'Invalid number of classes.'
+
+			return fc4
+
+	def _create_narrow_and_deep_model_with_diamond_shape(self, input_tensor, is_training, num_classes, max_neuron_count):
+		num_neurons = math.ceil((max_neuron_count / 2.0) ** (1.0 / 3.0))
+
+		with tf.variable_scope('fc1', reuse=tf.AUTO_REUSE):
+			fc1 = tf.layers.flatten(input_tensor, name='flatten')
+
+			fc1 = tf.layers.dense(fc1, num_neurons, activation=tf.nn.relu, name='fc')
+			#fc1 = tf.layers.dropout(fc1, rate=0.25, training=is_training, name='dropout')
+
+		with tf.variable_scope('fc2', reuse=tf.AUTO_REUSE):
+			fc2 = tf.layers.dense(fc1, num_neurons * 2, activation=tf.nn.relu, name='fc')
+			#fc2 = tf.layers.dropout(fc2, rate=0.25, training=is_training, name='dropout')
+
+		with tf.variable_scope('fc3', reuse=tf.AUTO_REUSE):
+			fc3 = tf.layers.dense(fc2, num_neurons, activation=tf.nn.relu, name='fc')
+			#fc3 = tf.layers.dropout(fc3, rate=0.25, training=is_training, name='dropout')
+
+		with tf.variable_scope('fc4', reuse=tf.AUTO_REUSE):
+			if 1 == num_classes:
+				fc4 = tf.layers.dense(fc3, 1, activation=tf.sigmoid, name='fc')
+				#fc4 = tf.layers.dense(fc3, 1, activation=tf.sigmoid, activity_regularizer=tf.contrib.layers.l2_regularizer(0.0001), name='fc')
+			elif num_classes >= 2:
+				fc4 = tf.layers.dense(fc3, num_classes, activation=tf.nn.softmax, name='fc')
+				#fc4 = tf.layers.dense(fc3, num_classes, activation=tf.nn.softmax, activity_regularizer=tf.contrib.layers.l2_regularizer(0.0001), name='fc')
+			else:
+				assert num_classes > 0, 'Invalid number of classes.'
+
+			return fc4
+
+	def _create_narrow_and_deep_model_with_hourglass_shape(self, input_tensor, is_training, num_classes, max_neuron_count):
+		num_neurons = math.ceil((max_neuron_count / 4.0) ** (1.0 / 3.0))
+
+		with tf.variable_scope('fc1', reuse=tf.AUTO_REUSE):
+			fc1 = tf.layers.flatten(input_tensor, name='flatten')
+
+			fc1 = tf.layers.dense(fc1, num_neurons * 2, activation=tf.nn.relu, name='fc')
+			#fc1 = tf.layers.dropout(fc1, rate=0.25, training=is_training, name='dropout')
+
+		with tf.variable_scope('fc2', reuse=tf.AUTO_REUSE):
+			fc2 = tf.layers.dense(fc1, num_neurons, activation=tf.nn.relu, name='fc')
+			#fc2 = tf.layers.dropout(fc2, rate=0.25, training=is_training, name='dropout')
+
+		with tf.variable_scope('fc3', reuse=tf.AUTO_REUSE):
+			fc3 = tf.layers.dense(fc2, num_neurons * 2, activation=tf.nn.relu, name='fc')
+			#fc3 = tf.layers.dropout(fc3, rate=0.25, training=is_training, name='dropout')
+
+		with tf.variable_scope('fc4', reuse=tf.AUTO_REUSE):
+			if 1 == num_classes:
+				fc4 = tf.layers.dense(fc3, 1, activation=tf.sigmoid, name='fc')
+				#fc4 = tf.layers.dense(fc3, 1, activation=tf.sigmoid, activity_regularizer=tf.contrib.layers.l2_regularizer(0.0001), name='fc')
+			elif num_classes >= 2:
+				fc4 = tf.layers.dense(fc3, num_classes, activation=tf.nn.softmax, name='fc')
+				#fc4 = tf.layers.dense(fc3, num_classes, activation=tf.nn.softmax, activity_regularizer=tf.contrib.layers.l2_regularizer(0.0001), name='fc')
+			else:
+				assert num_classes > 0, 'Invalid number of classes.'
+
+			return fc4
---
+++ python/test/machine_learning/tensorflow/mnist_mlp_test.py
@@ -0,0 +1,322 @@
+# Path to libcudnn.so.
+#export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+#--------------------
+import os, sys
+if 'posix' == os.name:
+	swl_python_home_dir_path = '/home/sangwook/work/SWL_github/python'
+	lib_home_dir_path = '/home/sangwook/lib_repo/python'
+else:
+	swl_python_home_dir_path = 'D:/work/SWL_github/python'
+	lib_home_dir_path = 'D:/lib_repo/python'
+	#lib_home_dir_path = 'D:/lib_repo/python/rnd'
+#sys.path.append('../../../src')
+sys.path.append(swl_python_home_dir_path + '/src')
+sys.path.append(lib_home_dir_path + '/tf_cnnvis_github')
+
+#os.chdir(swl_python_home_dir_path + '/test/machine_learning/tensorflow')
+
+#--------------------
+import numpy as np
+import tensorflow as tf
+from mnist_mlp import MnistMLP
+from simple_neural_net_trainer import SimpleNeuralNetTrainer
+from swl.machine_learning.tensorflow.neural_net_evaluator import NeuralNetEvaluator
+from swl.machine_learning.tensorflow.neural_net_inferrer import NeuralNetInferrer
+import swl.machine_learning.util as swl_ml_util
+import time
+
+#%%------------------------------------------------------------------
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+def load_data(data_dir_path, shape):
+	mnist = input_data.read_data_sets(data_dir_path, one_hot=True)
+
+	train_images = np.reshape(mnist.train.images, (-1,) + shape)
+	train_labels = np.round(mnist.train.labels).astype(np.int)
+	test_images = np.reshape(mnist.test.images, (-1,) + shape)
+	test_labels = np.round(mnist.test.labels).astype(np.int)
+
+	return train_images, train_labels, test_images, test_labels
+
+def preprocess_data(data, labels, num_classes, axis=0):
+	if data is not None:
+		# Preprocessing (normalization, standardization, etc.).
+		#data = data.astype(np.float32)
+		#data /= 255.0
+		#data = (data - np.mean(data, axis=axis)) / np.std(data, axis=axis)
+		#data = np.reshape(data, data.shape + (1,))
+		pass
+
+	if labels is not None:
+		# One-hot encoding (num_examples, height, width) -> (num_examples, height, width, num_classes).
+		#labels = to_one_hot_encoding(labels, num_classes).astype(np.uint8)
+		pass
+
+	return data, labels
+
+def train_neural_net(session, nnTrainer, train_images, train_labels, val_images, val_labels, batch_size, num_epochs, shuffle, does_resume_training, saver, output_dir_path, model_dir_path, train_summary_dir_path, val_summary_dir_path):
+	if does_resume_training:
+		print('[SWL] Info: Resume training...')
+
+		# Load a model.
+		# REF [site] >> https://www.tensorflow.org/programmers_guide/saved_model
+		# REF [site] >> http://cv-tricks.com/tensorflow-tutorial/save-restore-tensorflow-models-quick-complete-tutorial/
+		ckpt = tf.train.get_checkpoint_state(model_dir_path)
+		saver.restore(session, ckpt.model_checkpoint_path)
+		#saver.restore(session, tf.train.latest_checkpoint(model_dir_path))
+		print('[SWL] Info: Restored a model.')
+	else:
+		print('[SWL] Info: Start training...')
+
+	start_time = time.time()
+	history = nnTrainer.train(session, train_images, train_labels, val_images, val_labels, batch_size, num_epochs, shuffle, saver=saver, model_save_dir_path=model_dir_path, train_summary_dir_path=train_summary_dir_path, val_summary_dir_path=val_summary_dir_path)
+	print('\tTraining time = {}'.format(time.time() - start_time))
+
+	# Display results.
+	#swl_ml_util.display_train_history(history)
+	if output_dir_path is not None:
+		swl_ml_util.save_train_history(history, output_dir_path)
+	print('[SWL] Info: End training...')
+
+def evaluate_neural_net(session, nnEvaluator, val_images, val_labels, batch_size, saver=None, model_dir_path=None):
+	num_val_examples = 0
+	if val_images is not None and val_labels is not None:
+		if val_images.shape[0] == val_labels.shape[0]:
+			num_val_examples = val_images.shape[0]
+
+	if num_val_examples > 0:
+		if saver is not None and model_dir_path is not None:
+			# Load a model.
+			# REF [site] >> https://www.tensorflow.org/programmers_guide/saved_model
+			# REF [site] >> http://cv-tricks.com/tensorflow-tutorial/save-restore-tensorflow-models-quick-complete-tutorial/
+			ckpt = tf.train.get_checkpoint_state(model_dir_path)
+			saver.restore(session, ckpt.model_checkpoint_path)
+			#saver.restore(session, tf.train.latest_checkpoint(model_dir_path))
+			print('[SWL] Info: Loaded a model.')
+
+		print('[SWL] Info: Start evaluation...')
+		start_time = time.time()
+		val_loss, val_acc = nnEvaluator.evaluate(session, val_images, val_labels, batch_size)
+		print('\tEvaluation time = {}'.format(time.time() - start_time))
+		print('\tValidation loss = {}, validation accurary = {}'.format(val_loss, val_acc))
+		print('[SWL] Info: End evaluation...')
+	else:
+		print('[SWL] Error: The number of validation images is not equal to that of validation labels.')
+
+def infer_by_neural_net(session, nnInferrer, test_images, test_labels, num_classes, batch_size, saver=None, model_dir_path=None):
+	num_inf_examples = 0
+	if test_images is not None and test_labels is not None:
+		if test_images.shape[0] == test_labels.shape[0]:
+			num_inf_examples = test_images.shape[0]
+
+	if num_inf_examples > 0:
+		if saver is not None and model_dir_path is not None:
+			# Load a model.
+			# REF [site] >> https://www.tensorflow.org/programmers_guide/saved_model
+			# REF [site] >> http://cv-tricks.com/tensorflow-tutorial/save-restore-tensorflow-models-quick-complete-tutorial/
+			ckpt = tf.train.get_checkpoint_state(model_dir_path)
+			saver.restore(session, ckpt.model_checkpoint_path)
+			#saver.restore(session, tf.train.latest_checkpoint(model_dir_path))
+			print('[SWL] Info: Loaded a model.')
+
+		print('[SWL] Info: Start inferring...')
+		start_time = time.time()
+		inferences = nnInferrer.infer(session, test_images, batch_size)
+		print('\tInference time = {}'.format(time.time() - start_time))
+
+		if num_classes >= 2:
+			inferences = np.argmax(inferences, -1)
+			groundtruths = np.argmax(test_labels, -1)
+		else:
+			inferences = np.around(inferences)
+			groundtruths = test_labels
+		correct_estimation_count = np.count_nonzero(np.equal(inferences, groundtruths))
+
+		print('\tAccurary = {} / {} = {}'.format(correct_estimation_count, groundtruths.size, correct_estimation_count / groundtruths.size))
+		print('[SWL] Info: End inferring...')
+	else:
+		print('[SWL] Error: The number of test images is not equal to that of test labels.')
+
+#%%------------------------------------------------------------------
+
+import datetime
+#from keras import backend as K
+
+def make_dir(dir_path):
+	if not os.path.exists(dir_path):
+		try:
+			os.makedirs(dir_path)
+		except OSError as ex:
+			if os.errno.EEXIST != ex.errno:
+				raise
+
+def create_mnist_mlp(input_shape, output_shape):
+	model_type = 4  # {1, 4}.
+	max_neuron_count = 200000
+	return MnistMLP(input_shape, output_shape, model_type, max_neuron_count)
+
+def main():
+	#np.random.seed(7)
+
+	does_need_training = True
+	does_resume_training = False
+
+	#--------------------
+	# Prepare directories.
+
+	output_dir_prefix = 'mnist_mlp'
+	output_dir_suffix = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
+	#output_dir_suffix = '20180302T155710'
+
+	output_dir_path = './{}_{}'.format(output_dir_prefix, output_dir_suffix)
+	model_dir_path = '{}/model'.format(output_dir_path)
+	inference_dir_path = '{}/inference'.format(output_dir_path)
+	train_summary_dir_path = '{}/train_log'.format(output_dir_path)
+	val_summary_dir_path = '{}/val_log'.format(output_dir_path)
+
+	make_dir(model_dir_path)
+	make_dir(inference_dir_path)
+	make_dir(train_summary_dir_path)
+	make_dir(val_summary_dir_path)
+
+	#--------------------
+	# Prepare data.
+
+	if 'posix' == os.name:
+		#data_home_dir_path = '/home/sangwook/my_dataset'
+		data_home_dir_path = '/home/HDD1/sangwook/my_dataset'
+	else:
+		data_home_dir_path = 'D:/dataset'
+	data_dir_path = data_home_dir_path + '/pattern_recognition/mnist/0_original'
+
+	num_classes = 10
+	input_shape = (None, 28, 28, 1)  # 784 = 28 * 28.
+	output_shape = (None, num_classes)
+
+	train_images, train_labels, test_images, test_labels = load_data(data_dir_path, input_shape[1:])
+
+	# Pre-process.
+	#train_images, train_labels = preprocess_data(train_images, train_labels, num_classes)
+	#test_images, test_labels = preprocess_data(test_images, test_labels, num_classes)
+
+	#--------------------
+	# Create models, sessions, and graphs.
+
+	# Create graphs.
+	if does_need_training:
+		train_graph = tf.Graph()
+	eval_graph = tf.Graph()
+	infer_graph = tf.Graph()
+
+	if does_need_training:
+		with train_graph.as_default():
+			#K.set_learning_phase(1)  # Set the learning phase to 'train'. (Required)
+
+			# Create a model.
+			mlpModelForTraining = create_mnist_mlp(input_shape, output_shape)
+			mlpModelForTraining.create_training_model()
+
+			# Create a trainer.
+			initial_epoch = 0
+			nnTrainer = SimpleNeuralNetTrainer(mlpModelForTraining, initial_epoch)
+
+			# Create a saver.
+			#	Save a model every 2 hours and maximum 5 latest models are saved.
+			train_saver = tf.train.Saver(max_to_keep=5, keep_checkpoint_every_n_hours=2)
+
+			initializer = tf.global_variables_initializer()
+
+	with eval_graph.as_default():
+		#K.set_learning_phase(0)  # Set the learning phase to 'test'. (Required)
+
+		# Create a model.
+		mlpModelForEvaluation = create_mnist_mlp(input_shape, output_shape)
+		mlpModelForEvaluation.create_evaluation_model()
+
+		# Create an evaluator.
+		nnEvaluator = NeuralNetEvaluator(mlpModelForEvaluation)
+
+		# Create a saver.
+		eval_saver = tf.train.Saver()
+
+	with infer_graph.as_default():
+		#K.set_learning_phase(0)  # Set the learning phase to 'test'. (Required)
+
+		# Create a model.
+		mlpModelForInference = create_mnist_mlp(input_shape, output_shape)
+		mlpModelForInference.create_inference_model()
+
+		# Create an inferrer.
+		nnInferrer = NeuralNetInferrer(mlpModelForInference)
+
+		# Create a saver.
+		infer_saver = tf.train.Saver()
+
+	# Create sessions.
+	config = tf.ConfigProto()
+	#config.allow_soft_placement = True
+	config.log_device_placement = True
+	config.gpu_options.allow_growth = True
+	#config.gpu_options.per_process_gpu_memory_fraction = 0.4  # Only allocate 40% of the total memory of each GPU.
+
+	if does_need_training:
+		train_session = tf.Session(graph=train_graph, config=config)
+	eval_session = tf.Session(graph=eval_graph, config=config)
+	infer_session = tf.Session(graph=infer_graph, config=config)
+
+	# Initialize.
+	if does_need_training:
+		train_session.run(initializer)
+
+	#%%------------------------------------------------------------------
+	# Train.
+
+	batch_size = 128  # Number of samples per gradient update.
+	num_epochs = 20  # Number of times to iterate over training data.	
+	shuffle = True
+
+	if does_need_training:
+		total_elapsed_time = time.time()
+		with train_session.as_default() as sess:
+			with sess.graph.as_default():
+				#K.set_session(sess)
+				#K.set_learning_phase(1)  # Set the learning phase to 'train'.
+				train_neural_net(sess, nnTrainer, train_images, train_labels, test_images, test_labels, batch_size, num_epochs, shuffle, does_resume_training, train_saver, output_dir_path, model_dir_path, train_summary_dir_path, val_summary_dir_path)
+		print('\tTotal training time = {}'.format(time.time() - total_elapsed_time))
+
+	#%%------------------------------------------------------------------
+	# Evaluate and infer.
+
+	total_elapsed_time = time.time()
+	with eval_session.as_default() as sess:
+		with sess.graph.as_default():
+			#K.set_session(sess)
+			#K.set_learning_phase(0)  # Set the learning phase to 'test'.
+			evaluate_neural_net(sess, nnEvaluator, test_images, test_labels, batch_size, eval_saver, model_dir_path)
+	print('\tTotal evaluation time = {}'.format(time.time() - total_elapsed_time))
+
+	total_elapsed_time = time.time()
+	with infer_session.as_default() as sess:
+		with sess.graph.as_default():
+			#K.set_session(sess)
+			#K.set_learning_phase(0)  # Set the learning phase to 'test'.
+			infer_by_neural_net(sess, nnInferrer, test_images, test_labels, num_classes, batch_size, infer_saver, model_dir_path)
+	print('\tTotal inference time = {}'.format(time.time() - total_elapsed_time))
+
+	#--------------------
+	# Close sessions.
+
+	if does_need_training:
+		train_session.close()
+		train_session = None
+	eval_session.close()
+	eval_session = None
+	infer_session.close()
+	infer_session = None
+
+#%%------------------------------------------------------------------
+
+if '__main__' == __name__:
+	main()
