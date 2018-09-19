49ef063a38f330db308757c5eb606b8a0accfadb
Sang-Wook Lee <sangwook236@gmail.com>
2018-09-20 03:04:39 +0900

    [Add, Python] Implemented top-k and bottom-k algorithms.

--- python/src/swl/util/util.py
+++ python/src/swl/util/util.py
@@ -7,3 +7,27 @@ def find_most_frequent_value(arr):
 	"""
 	counts = np.bincount(arr)
 	return np.argmax(counts)
+
+def top_k_values(iterable, k):
+	# In ascending order.
+	#return sorted(iterable)[-k:]  # Top-k values.
+	# In descending order.
+	return sorted(iterable, reverse=True)[:k]  # Top-k values.
+
+def bottom_k_values(iterable, k):
+	# In ascending order.
+	return sorted(iterable)[:k]  # Bottom-k values.
+	# In descending order.
+	#return sorted(iterable, reverse=True)[-k:]  # Bottom-k values.
+
+def top_k_indices(iterable, k):
+	# In ascending order.
+	#return sorted(range(len(iterable)), key=lambda i: iterable[i])[-k:]  # Top-k indices.
+	# In descending order.
+	return sorted(range(len(iterable)), key=lambda i: iterable[i], reverse=True)[:k]  # Top-k indices.
+
+def bottom_k_indices(iterable, k):
+	# In ascending order.
+	return sorted(range(len(iterable)), key=lambda i: iterable[i])[:k]  # Bottom-k indices.
+	# In descending order.
+	#return sorted(range(len(iterable)), key=lambda i: iterable[i], reverse=True)[-k:]  # Bottom-k indices.
