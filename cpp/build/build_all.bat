3deb2e3d6ed4068bfd4e293a153f46ec6c4cf7a7
Sang-Wook Lee <sangwook236@gmail.com>
2019-01-05 19:32:49 +0900

    [Update, Script] Enhanced build_all.bat.

--- cpp/build/build_all.bat
+++ cpp/build/build_all.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal
 
-cmake ..
-rem cmake -DCMAKE_BUILD_TYPE=Release ..
-rem cmake -DCMAKE_BUILD_TYPE=Debug ..
+call cmake ..
+rem call cmake -DCMAKE_BUILD_TYPE=Release ..
+rem call cmake -DCMAKE_BUILD_TYPE=Debug ..
 
-make -j4
-make -j4 test
-make -j4 doc
+call make -j4
+call make -j4 test
+call make -j4 doc
 
 endlocal
 echo on
