561d643189c78ed6e88f25c6f74db88919fb31d2
Sang-Wook Lee <sangwook236@gmail.com>
2019-01-21 20:51:42 +0900

    [Add, Python] Implemented a function which loads npy files in a directory.

--- cpp/src/base/String.cpp
+++ cpp/src/base/String.cpp
@@ -1,9 +1,9 @@
 #include "swl/Config.h"
 #include "swl/base/String.h"
-#include <boost/smart_ptr.hpp>
 #if defined(_WIN64) || defined(WIN64) || defined(_WIN32) || defined(WIN32)
 #include <windows.h>
 #endif
+#include <memory>
 #include <clocale>
 
 
@@ -16,26 +16,26 @@
 namespace swl {
 
 //-----------------------------------------------------------------------------------
-//	string
+// String.
 
 /*static*/ std::string String::wcs2mbs(const std::wstring &wcstr)
 {
 #if defined(_WIN64) || defined(WIN64) || defined(_WIN32) || defined(WIN32)
 	const int len = WideCharToMultiByte(CP_ACP, 0, wcstr.c_str(), -1, NULL, 0, NULL, NULL);
-	boost::scoped_array<char> mbstr(new char [len]);
+	std::unique_ptr<char []> mbstr(new char [len]);
 	const int len2 = WideCharToMultiByte(CP_ACP, 0, wcstr.c_str(), -1, mbstr.get(), (int)len, NULL, NULL);
 	//assert(wcstr.length() == len2);
 #else
 	const size_t len = wcstr.length() * 2 + 1;
-	boost::scoped_array<char> mbstr(new char [len]);
+	std::unique_ptr<char []> mbstr(new char [len]);
 
-	// method 1
-	setlocale(LC_ALL, "korea");
-	wcstombs(mbstr.get(), wcstr.c_str(), len);
+	// Method 1.
+	std::setlocale(LC_ALL, "korea");
+	std::wcstombs(mbstr.get(), wcstr.c_str(), len);
 
-	// method 2
+	// Method 2.
 	//_locale_t locale = _create_locale(LC_ALL, "korea");
-	//wcstombs_l(mbstr.get(), wcstr.c_str(), len, locale);
+	//std::wcstombs_l(mbstr.get(), wcstr.c_str(), len, locale);
 #endif
 
 	return std::string(mbstr.get());
@@ -45,27 +45,27 @@ namespace swl {
 {
 #if defined(_WIN64) || defined(WIN64) || defined(_WIN32) || defined(WIN32)
 	const int len = MultiByteToWideChar(CP_ACP, 0, mbstr.c_str(), -1, NULL, 0);
-	boost::scoped_array<wchar_t> wcstr(new wchar_t [len]);
+	std::unique_ptr<wchar_t []> wcstr(new wchar_t [len]);
 	const int len2 = MultiByteToWideChar(CP_ACP, 0, mbstr.c_str(), -1, wcstr.get(), (int)len);
-	//mbstr(wcstr.length() == len2);
+	//assert(wcstr.length() == len2);
 #else
 	const size_t len = mbstr.length() * 2 + 1;
-	boost::scoped_array<wchar_t> wcstr(new wchar_t [len]);
+	std::unique_ptr<wchar_t []> wcstr(new wchar_t [len]);
 
-	// method 1
-	setlocale(LC_ALL, "korea");
-	mbstowcs(wcstr.get(), mbstr.c_str(), len);
+	// Method 1.
+	std::setlocale(LC_ALL, "korea");
+	std::mbstowcs(wcstr.get(), mbstr.c_str(), len);
 
-	// method 2
+	// Method 2.
 	//_locale_t locale = _create_locale(LC_ALL, "korea");
-	//mbstowcs_l(wcstr.get(), mbstr.c_str(), len, locale);
+	//std::mbstowcs_l(wcstr.get(), mbstr.c_str(), len, locale);
 #endif
 
 	return std::wstring(wcstr.get());
 }
 
 //-----------------------------------------------------------------------------------
-//  byte data converter
+// Byte data converter.
 
 /*static*/ unsigned char String::dec2ascii(const unsigned char dec)
 {
--- python/src/swl/util/util.py
+++ python/src/swl/util/util.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 import numpy as np
 
 def find_most_frequent_value(arr):
@@ -40,3 +40,17 @@ def make_dir(dir_path):
 		except OSError as ex:
 			if os.errno.EEXIST != ex.errno:
 				raise
+
+def load_npy_files_in_directory(dir_path, file_prefix, file_suffix):
+	file_extension = 'npy'
+	arr_list = list()
+	if dir_path is not None:
+		for root, dirnames, filenames in os.walk(dir_path):
+			filenames.sort()
+			for filename in filenames:
+				if re.search('^' + file_prefix, filename) and re.search(file_suffix + '\.' + file_extension + '$', filename):
+					filepath = os.path.join(root, filename)
+					arr = np.load(filepath)
+					arr_list.append(arr)
+			break  # Do not include subdirectories.
+	return arr_list
