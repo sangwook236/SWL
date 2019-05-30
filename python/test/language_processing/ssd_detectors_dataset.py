b423cd365a4875cce1674577a7847b52531b9796
Sang-Wook Lee <sangwook236@gmail.com>
2019-05-30 22:57:58 +0900

    [Update, Python] Fixed the algorithm to generate dataset for ssd_detectors library.

--- python/test/language_processing/ssd_detectors_dataset.py
+++ python/test/language_processing/ssd_detectors_dataset.py
@@ -30,111 +30,137 @@ class GTUtility(BaseGTUtility):
 	"""Utility for my scene text dataset.
 
 	# Arguments
-		data_path: Path to ground truth and image data.
+		data_dir_path: Path to ground truth and image data.
 		max_slope: Maximum slope of text lines. Boxes with slope lower 
 			then max_slope degrees are rejected.
 		polygon: Return oriented boxes defined by their four corner points.
 			Required by SegLink...
 	"""
-	def __init__(self, my_scene_text_data_path, my_scene_text_json_filename, rrc_mlt_2019_data_path, e2e_mlt_data_path, max_slope=None, polygon=False):
+	def __init__(self, use_my_scene_text_dataset, use_rrc_mlt_2019_dataset, use_e2e_mlt_dataset, max_slope=None, polygon=False):
 		self.classes = ['Background', 'Text']
 
-		datasets = list()
-		if my_scene_text_data_path is not None:
-			img_filepaths, mask_filepaths, gt_texts, gt_boxes = self.load_my_scene_text_dataset(my_scene_text_data_path, my_scene_text_json_filename)
-			datasets.append((my_scene_text_data_path, (img_filepaths, gt_texts, gt_boxes)))
-		if rrc_mlt_2019_data_path is not None:
-			img_filepaths, _, gt_texts, gt_boxes = self.load_rrc_mlt_2019_dataset(rrc_mlt_2019_data_path)
-			datasets.append((rrc_mlt_2019_data_path, (img_filepaths, gt_texts, gt_boxes)))
-		if e2e_mlt_data_path is not None:
-			img_filepaths, _, gt_texts, gt_boxes = self.load_e2e_mlt_dataset(e2e_mlt_data_path)
-			datasets.append((e2e_mlt_data_path, (img_filepaths, gt_texts, gt_boxes)))
+		# Data directory structure:
+		#	scene_text_dataset
+		#		e2e_mlt
+		#			Korean
+		#				*.jpg
+		#				*.txt
+		#		mask
+		#			*.png
+		#		rrc_icdar
+		#			mlt_2019
+		#				ImagesPart2
+		#					*.jpg / *.png / *.gif
+		#				train_gt_t13
+		#					*.txt
+		#		scene
+		#			*.png
+		#		scene_text_dataset.json
+		data_dir_path = './scene_text_dataset'
+
+		img_filepaths, gt_texts, gt_boxes = list(), list(), list()
+		if use_my_scene_text_dataset:
+			img_filepaths0, mask_filepaths0, gt_texts0, gt_boxes0 = self.load_my_scene_text_dataset(data_dir_path)
+			img_filepaths.extend(img_filepaths0)
+			gt_texts.extend(gt_texts0)
+			gt_boxes.extend(gt_boxes0)
+		if use_rrc_mlt_2019_dataset:
+			img_filepaths0, mask_filepaths0, gt_texts0, gt_boxes0 = self.load_rrc_mlt_2019_dataset(data_dir_path)
+			img_filepaths.extend(img_filepaths0)
+			gt_texts.extend(gt_texts0)
+			gt_boxes.extend(gt_boxes0)
+		if use_e2e_mlt_dataset:
+			img_filepaths0, mask_filepaths0, gt_texts0, gt_boxes0 = self.load_e2e_mlt_dataset(data_dir_path)
+			img_filepaths.extend(img_filepaths0)
+			gt_texts.extend(gt_texts0)
+			gt_boxes.extend(gt_boxes0)
+		del img_filepaths0, mask_filepaths0, gt_texts0, gt_boxes0
 
 		#--------------------
 		print('Processing samples...')
 		self.image_names, self.data, self.text = list(), list(), list()
-		for data_path, [img_filepaths, gt_texts, gt_boxes] in datasets:
-			for img_filepath, text, boxes in zip(img_filepaths, gt_texts, gt_boxes):
-				img = cv2.imread(os.path.join(data_path, img_filepath))
-				img_height, img_width = img.shape[:2]
+		for img_filepath, text, boxes in zip(img_filepaths, gt_texts, gt_boxes):
+			img = cv2.imread(os.path.join(data_dir_path, img_filepath))
+			img_height, img_width = img.shape[:2]
 
-				#boxes = np.array(boxes)
-				boxes[:,:,0] /= img_width
-				boxes[:,:,1] /= img_height
-				boxes = boxes.reshape(boxes.shape[0], -1)
+			#boxes = np.array(boxes)
+			boxes[:,:,0] /= img_width
+			boxes[:,:,1] /= img_height
+			boxes = boxes.reshape(boxes.shape[0], -1)
 
-				# Fix some bugs in the SynthText dataset.
-				eps = 1e-3
-				p1, p2, p3, p4 = boxes[:,0:2], boxes[:,2:4], boxes[:,4:6],boxes[:,6:8]
-				# Fix twisted boxes (897 boxes, 0.012344 %).
-				if True:
-					mask = np.linalg.norm(p1 + p2 - p3 - p4, axis=1) < eps
-					boxes[mask] = np.concatenate([p1[mask], p3[mask], p2[mask], p4[mask]], axis=1)
-				# Filter out bad boxes (528 boxes, 0.007266 %).
-				if True:
-					mask = np.ones(len(boxes), dtype=np.bool)
-					# Filter boxes with zero width (173 boxes, 0.002381 %).
-					boxes_w = np.linalg.norm(p1-p2, axis=1)
-					boxes_h = np.linalg.norm(p2-p3, axis=1)
-					mask = np.logical_and(mask, boxes_w > eps)
-					mask = np.logical_and(mask, boxes_h > eps)
-					# Filter boxes that are too large (62 boxes, 0.000853 %).
-					mask = np.logical_and(mask, np.all(boxes > -1, axis=1))
-					mask = np.logical_and(mask, np.all(boxes < 2, axis=1))
-					# Filter boxes with all vertices outside the image (232 boxes, 0.003196 %).
-					boxes_x = boxes[:,0::2]
-					boxes_y = boxes[:,1::2]
-					mask = np.logical_and(mask, 
-							np.sum(np.logical_or(np.logical_or(boxes_x < 0, boxes_x > 1), 
-									np.logical_or(boxes_y < 0, boxes_y > 1)), axis=1) < 4)
-					# Filter boxes with center outside the image (336 boxes, 0.004624 %).
-					boxes_x_mean = np.mean(boxes[:,0::2], axis=1)
-					boxes_y_mean = np.mean(boxes[:,1::2], axis=1)
-					mask = np.logical_and(mask, np.logical_and(boxes_x_mean > 0, boxes_x_mean < 1))
-					mask = np.logical_and(mask, np.logical_and(boxes_y_mean > 0, boxes_y_mean < 1))
-					boxes = boxes[mask]
-					text = np.asarray(text)[mask]
+			# Fix some bugs in the SynthText dataset.
+			eps = 1e-3
+			p1, p2, p3, p4 = boxes[:,0:2], boxes[:,2:4], boxes[:,4:6],boxes[:,6:8]
+			# Fix twisted boxes (897 boxes, 0.012344 %).
+			if True:
+				mask = np.linalg.norm(p1 + p2 - p3 - p4, axis=1) < eps
+				boxes[mask] = np.concatenate([p1[mask], p3[mask], p2[mask], p4[mask]], axis=1)
+			# Filter out bad boxes (528 boxes, 0.007266 %).
+			if True:
+				mask = np.ones(len(boxes), dtype=np.bool)
+				# Filter boxes with zero width (173 boxes, 0.002381 %).
+				boxes_w = np.linalg.norm(p1-p2, axis=1)
+				boxes_h = np.linalg.norm(p2-p3, axis=1)
+				mask = np.logical_and(mask, boxes_w > eps)
+				mask = np.logical_and(mask, boxes_h > eps)
+				# Filter boxes that are too large (62 boxes, 0.000853 %).
+				mask = np.logical_and(mask, np.all(boxes > -1, axis=1))
+				mask = np.logical_and(mask, np.all(boxes < 2, axis=1))
+				# Filter boxes with all vertices outside the image (232 boxes, 0.003196 %).
+				boxes_x = boxes[:,0::2]
+				boxes_y = boxes[:,1::2]
+				mask = np.logical_and(mask, 
+						np.sum(np.logical_or(np.logical_or(boxes_x < 0, boxes_x > 1), 
+								np.logical_or(boxes_y < 0, boxes_y > 1)), axis=1) < 4)
+				# Filter boxes with center outside the image (336 boxes, 0.004624 %).
+				boxes_x_mean = np.mean(boxes[:,0::2], axis=1)
+				boxes_y_mean = np.mean(boxes[:,1::2], axis=1)
+				mask = np.logical_and(mask, np.logical_and(boxes_x_mean > 0, boxes_x_mean < 1))
+				mask = np.logical_and(mask, np.logical_and(boxes_y_mean > 0, boxes_y_mean < 1))
+				boxes = boxes[mask]
+				text = np.asarray(text)[mask]
 
-				# Only boxes with slope below max_slope.
-				if not max_slope == None:
-					angles = np.arctan(np.divide(boxes[:,2]-boxes[:,0], boxes[:,3]-boxes[:,1]))
-					angles[angles < 0] += np.pi
-					angles = angles/np.pi*180-90
-					boxes = boxes[np.abs(angles) < max_slope]
+			# Only boxes with slope below max_slope.
+			if not max_slope == None:
+				angles = np.arctan(np.divide(boxes[:,2]-boxes[:,0], boxes[:,3]-boxes[:,1]))
+				angles[angles < 0] += np.pi
+				angles = angles/np.pi*180-90
+				boxes = boxes[np.abs(angles) < max_slope]
 
-				# Only images with boxes.
-				if len(boxes) == 0:
-					continue
+			# Only images with boxes.
+			if len(boxes) == 0:
+				continue
 
-				if not polygon:
-					xmax = np.max(boxes[:,0::2], axis=1)
-					xmin = np.min(boxes[:,0::2], axis=1)
-					ymax = np.max(boxes[:,1::2], axis=1)
-					ymin = np.min(boxes[:,1::2], axis=1)
-					boxes = np.array([xmin, ymin, xmax, ymax]).T
+			if not polygon:
+				xmax = np.max(boxes[:,0::2], axis=1)
+				xmin = np.min(boxes[:,0::2], axis=1)
+				ymax = np.max(boxes[:,1::2], axis=1)
+				ymin = np.min(boxes[:,1::2], axis=1)
+				boxes = np.array([xmin, ymin, xmax, ymax]).T
 
-				# Append classes.
-				boxes = np.concatenate([boxes, np.ones([boxes.shape[0],1])], axis=1)
-				self.image_names.append(img_filepath)
-				self.data.append(boxes)
-				self.text.append(text)
+			# Append classes.
+			boxes = np.concatenate([boxes, np.ones([boxes.shape[0],1])], axis=1)
+			self.image_names.append(img_filepath)
+			self.data.append(boxes)
+			self.text.append(text)
 
 		self.init()
 
-	def load_my_scene_text_dataset(self, data_path, json_filename):
+	def load_my_scene_text_dataset(self, data_dir_path, json_filename):
 		print('Loading data (My scene text dataset)...')
 		start_time = time.time()
-		img_filepaths, mask_filepaths, gt_texts, gt_boxes = load_scene_text_dataset(data_path, json_filename)
+		json_filename = 'scene_text_dataset.json'
+		img_filepaths, mask_filepaths, gt_texts, gt_boxes = load_scene_text_dataset(data_dir_path, json_filename)
 		print('\tElapsed time = {}'.format(time.time() - start_time))
 
 		return img_filepaths, mask_filepaths, gt_texts, gt_boxes
 
 	# REF [function] >> rrc_mlt_2019_bounding_box_test() in rrc_dataset.py
-	def load_rrc_mlt_2019_dataset(self, data_path):
-		print('Loading file list (RRC MLT 2019)...')
+	def load_rrc_mlt_2019_dataset(self, data_dir_path):
+		print('Loading file list (RRC MLT 2019 dataset)...')
 		start_time = time.time()
-		img_filepaths = glob.glob(os.path.join(data_path, 'ImagesPart?/tr_img_*.*'), recursive=False)
-		gt_filepaths = glob.glob(os.path.join(data_path, 'train_gt_t13/tr_img_*.txt'), recursive=False)
+		img_filepaths = glob.glob(os.path.join(data_dir_path, 'rrc_icdar/mlt_2019/ImagesPart?/tr_img_*.*'), recursive=False)
+		gt_filepaths = glob.glob(os.path.join(data_dir_path, 'rrc_icdar/mlt_2019/train_gt_t13/tr_img_*.txt'), recursive=False)
 
 		class FilenameExtracter:
 			def __init__(self, base_dir_path):
@@ -144,8 +170,8 @@ class GTUtility(BaseGTUtility):
 				idx = filepath.rfind(self.base_dir_path) + len(self.base_dir_path) + 1
 				return filepath[idx:]
 
-		img_filepaths = list(map(FilenameExtracter(data_path), img_filepaths))
-		gt_filepaths = list(map(FilenameExtracter(data_path), gt_filepaths))
+		img_filepaths = list(map(FilenameExtracter(data_dir_path), img_filepaths))
+		gt_filepaths = list(map(FilenameExtracter(data_dir_path), gt_filepaths))
 
 		if True:
 			class FileFilter:
@@ -171,18 +197,18 @@ class GTUtility(BaseGTUtility):
 			return
 
 		#--------------------
-		print('Loading data (RRC MLT 2019)...')
+		print('Loading data (RRC MLT 2019 dataset)...')
 		start_time = time.time()
 		gt_boxes, gt_texts = list(), list()
 		for img_filepath, gt_filepath in zip(img_filepaths, gt_filepaths):
-			img = cv2.imread(os.path.join(data_path, img_filepath))
+			img = cv2.imread(os.path.join(data_dir_path, img_filepath))
 			#height, width = img.shape[:2]
 
 			# REF [site] >> https://rrc.cvc.uab.es/?ch=8&com=tasks
 			#	x1,y1,x2,y2,x3,y3,x4,y4,script,transcription
 			#	Valid scripts are: "Arabic", "Latin", "Chinese", "Japanese", "Korean", "Bangla", "Symbols", "Mixed", "None".
 			boxes, texts = list(), list()
-			with open(os.path.join(data_path, gt_filepath), newline='', encoding='UTF-8') as csvfile:
+			with open(os.path.join(data_dir_path, gt_filepath), newline='', encoding='UTF-8') as csvfile:
 				reader = csv.reader(csvfile, delimiter=',')
 				for row in reader:
 					#if 10 != len(row):
@@ -200,11 +226,11 @@ class GTUtility(BaseGTUtility):
 		return img_filepaths, None, gt_texts, gt_boxes
 
 	# REF [function] >> bounding_box_test() in e2e_mlt_dataset.py
-	def load_e2e_mlt_dataset(self, data_path):
-		print('Loading file list (E2E-MLT)...')
+	def load_e2e_mlt_dataset(self, data_dir_path):
+		print('Loading file list (E2E-MLT dataset)...')
 		start_time = time.time()
-		img_filepaths = glob.glob(os.path.join(data_path, '*.jpg'), recursive=False)
-		gt_filepaths = glob.glob(os.path.join(data_path, '*.txt'), recursive=False)
+		img_filepaths = glob.glob(os.path.join(data_dir_path, 'e2e_mlt/Korean/*.jpg'), recursive=False)
+		gt_filepaths = glob.glob(os.path.join(data_dir_path, 'e2e_mlt/Korean/*.txt'), recursive=False)
 
 		class FilenameExtracter:
 			def __init__(self, base_dir_path):
@@ -214,8 +240,8 @@ class GTUtility(BaseGTUtility):
 				idx = filepath.rfind(self.base_dir_path) + len(self.base_dir_path) + 1
 				return filepath[idx:]
 
-		img_filepaths = list(map(FilenameExtracter(data_path), img_filepaths))
-		gt_filepaths = list(map(FilenameExtracter(data_path), gt_filepaths))
+		img_filepaths = list(map(FilenameExtracter(data_dir_path), img_filepaths))
+		gt_filepaths = list(map(FilenameExtracter(data_dir_path), gt_filepaths))
 
 		img_filepaths.sort(key=lambda filepath: os.path.basename(filepath))
 		gt_filepaths.sort(key=lambda filepath: os.path.basename(filepath))
@@ -226,17 +252,17 @@ class GTUtility(BaseGTUtility):
 			return
 
 		#--------------------
-		print('Loading data (E2E-MLT)...')
+		print('Loading data (E2E-MLT dataset)...')
 		start_time = time.time()
 		gt_boxes, gt_texts = list(), list()
 		for img_filepath, gt_filepath in zip(img_filepaths, gt_filepaths):
-			img = cv2.imread(os.path.join(data_path, img_filepath))
+			img = cv2.imread(os.path.join(data_dir_path, img_filepath))
 			height, width = img.shape[:2]
 			max_len = max(height, width)
 
 			# ?, center, size, angle, text.
 			boxes, texts = list(), list()
-			with open(os.path.join(data_path, gt_filepath), newline='', encoding='UTF-8') as csvfile:
+			with open(os.path.join(data_dir_path, gt_filepath), newline='', encoding='UTF-8') as csvfile:
 				reader = csv.reader(csvfile, delimiter=' ', quotechar=None)
 				for row in reader:
 					if 7 != len(row):
@@ -265,24 +291,9 @@ class GTUtility(BaseGTUtility):
 		return img_filepaths, None, gt_texts, gt_boxes
 
 def convert_scene_text_dataset_to_ssd_detectors():
-	if 'posix' == os.name:
-		data_home_dir_path = '/home/sangwook/my_dataset'
-	else:
-		data_home_dir_path = 'E:/dataset'
-
-	# REF [file] >> text_generator_test.py
-	scene_text_dataset_dir_path = './scene_text_dataset'
-	scene_text_dataset_json_filename = 'scene_text_dataset.json'
-	# REF [site] >> https://rrc.cvc.uab.es/?ch=15
-	rrc_mlt_2019_dir_path = data_home_dir_path + '/pattern_recognition/language_processing/rrc_icdar/0_download/mlt_2019'
-	# REF [site] >> https://github.com/MichalBusta/E2E-MLT
-	e2e_mlt_dir_path = data_home_dir_path + '/pattern_recognition/language_processing/e2e_mlt/0_download/Korean'
-
+	use_my_scene_text_dataset, use_rrc_mlt_2019_dataset, use_e2e_mlt_dataset = True, True, True
 	polygon = True
-	gt_util = GTUtility(scene_text_dataset_dir_path, scene_text_dataset_json_filename, rrc_mlt_2019_dir_path, e2e_mlt_dir_path, polygon=polygon)
-	#gt_util = GTUtility(scene_text_dataset_dir_path, scene_text_dataset_json_filename, None, None, polygon=polygon)
-	#gt_util = GTUtility(None, None, rrc_mlt_2019_dir_path, None, polygon=polygon)
-	#gt_util = GTUtility(None, None, None, e2e_mlt_dir_path, polygon=polygon)
+	gt_util = GTUtility(use_my_scene_text_dataset, use_rrc_mlt_2019_dataset, use_e2e_mlt_dataset, polygon=polygon)
 
 	file_name = 'gt_util_scene_text_seglink.pkl' if polygon else 'gt_util_scene_text.pkl'
 	print('Save to {}...'.format(file_name))
