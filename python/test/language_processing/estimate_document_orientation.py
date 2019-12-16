bc979f5e61d4cf6ad24c998bf0bcb1c0a0a0f620
Sang-Wook Lee <sangwook236@gmail.com>
2019-12-16 16:16:55 +0900

    [Update, Python] Applied eigen decomposition and image statistic to estimate document orientation.

--- python/test/language_processing/estimate_document_orientation.py
+++ python/test/language_processing/estimate_document_orientation.py
@@ -3,13 +3,11 @@
 
 import math, copy, glob
 import numpy as np
-import scipy
-import skimage
-from skimage.feature.peak import peak_local_max
+import scipy, scipy.fftpack
+import skimage, skimage.feature
 import cv2
 
-# REF [function] >> image_fft_example() in ${SWDT_PYTHON_HOME}/rnd/test/signal_processing/scipy_fftpack_fft.py
-def fft_based_analysis():
+def estimate_orientation_based_on_fft():
 	image_filepaths = glob.glob('/work/dataset/text/receipt_sminds/rotated/*.png')
 
 	for image_filepath in image_filepaths:
@@ -22,6 +20,7 @@ def fft_based_analysis():
 
 		#--------------------
 		# FFT.
+		# REF [function] >> image_fft_example() in ${SWDT_PYTHON_HOME}/rnd/test/signal_processing/scipy_fftpack_fft.py
 		if True:
 			img_fft = scipy.fftpack.fft2(img)
 		else:
@@ -56,21 +55,35 @@ def fft_based_analysis():
 
 		#--------------------
 		# Find peaks.
-		peaks = peak_local_max(img_fft_mag_normalized, min_distance=5, num_peaks=50)
-		tmp = copy.deepcopy(peaks[:,0])
-		peaks[:,0] = peaks[:,1]
-		peaks[:,1] = tmp
-		peaks = np.expand_dims(peaks, axis=1)
+		#peaks = skimage.feature.peak_local_max(img_fft_mag_normalized, min_distance=5, num_peaks=50)
+		#peaks = skimage.feature.peak_local_max(img_fft_mag_normalized, min_distance=10, num_peaks=50)
+		peaks = skimage.feature.peak_local_max(img_fft_mag_normalized, min_distance=1, num_peaks=500)
+		peaks = peaks[:,::-1]  # (y, x) -> (x, y).
 
 		rgb = cv2.cvtColor(np.round(img_fft_mag_normalized * 255).astype(np.uint8), cv2.COLOR_GRAY2BGR)
 		for pt in peaks:
-			cv2.circle(rgb, tuple(pt[0]), 2, (0, 0, 255), cv2.FILLED, cv2.LINE_AA)
+			cv2.circle(rgb, tuple(pt), 1, (255, 0, 0), cv2.FILLED, cv2.LINE_AA)
 
-		cv2.imshow('Peaks', rgb)
+		#--------------------
+		# Eigen analysis.
+		try:
+			peaks_centered = peaks - [cx, cy]
+			u, s, vh = np.linalg.svd(peaks_centered, full_matrices=True)
+			print('Document orientation (SVD) =', math.degrees(math.atan2(vh[0,1], vh[0,0])))
 
+			ll = min(img_fft_mag.shape[:2]) * 0.1
+			vh = np.round(vh * ll).astype(np.int32)
+			cv2.line(rgb, (cx, cy), tuple(vh[0] + (cx, cy)), (0, 255, 255), 3, cv2.LINE_AA)
+			cv2.line(rgb, (cx, cy), tuple(vh[1] + (cx, cy)), (255, 255, 0), 3, cv2.LINE_AA)
+		except np.linalg.LinAlgError:
+			print('np.linalg.LinAlgError raised.')
+			raise
+
+		#--------------------
+		peaks = np.expand_dims(peaks, axis=1)
 		if False:
 			#--------------------
-			# Detect lines.
+			# Hough transform.
 			lines_max = 3
 			threshold = 5
 			# TODO [check] >> I guess that rho has relations with image size.
@@ -98,19 +111,16 @@ def fft_based_analysis():
 					pt2 = (round(x0 + dx), round(y0 - dy))
 
 					cv2.line(rgb, pt1, pt2, (0, 0, 255), 1, cv2.LINE_AA);
-				for pt in peaks:
-					cv2.circle(rgb, tuple(pt[0]), 1, (255, 0, 0), cv2.FILLED, cv2.LINE_AA)
 
-				cv2.imshow('Line', rgb)
+				cv2.imshow('Result', rgb)
 			else:
 				print('No detected line.')
 		elif False:
 			peak_img = np.zeros(img_fft_mag_normalized.shape, dtype=np.uint8)
-			for pt in peaks:
-				peak_img[pt[0][1], pt[0][0]] = 255
+			peak_img[peaks[:,0,1],peaks[:,0,0]] = 255
 
 			#--------------------
-			# Detect lines.
+			# Hough transform.
 			# TODO [check] >> OpenCV implementation of Hough line transform may have some bugs.
 			#lines = cv2.HoughLines(peak_img, rho=5, theta=math.pi / 18, threshold=5, srn=0, stn=0, min_theta=0, max_theta=math.pi)
 			lines = cv2.HoughLines(peak_img, rho=5, theta=math.pi / 18, threshold=5, srn=0, stn=0, min_theta=-math.pi / 2, max_theta=math.pi / 2)
@@ -132,36 +142,40 @@ def fft_based_analysis():
 					pt2 = (round(x0 + dx), round(y0 - dy))
 
 					cv2.line(rgb, pt1, pt2, (0, 0, 255), 1, cv2.LINE_AA);
-				for pt in peaks:
-					cv2.circle(rgb, tuple(pt[0]), 1, (255, 0, 0), cv2.FILLED, cv2.LINE_AA)
 
-				cv2.imshow('Line', rgb)
+				cv2.imshow('Result', rgb)
 			else:
 				print('No detected line.')
 		elif True:
 			peak_img = np.zeros(img_fft_mag_normalized.shape, dtype=np.uint8)
-			for pt in peaks:
-				peak_img[pt[0][1], pt[0][0]] = 255
+			peak_img[peaks[:,0,1],peaks[:,0,0]] = 255
 
 			#--------------------
-			# Detect lines.
+			# Image statistics.
+			label_img = (peak_img > 0).astype(np.int)
+			props = skimage.measure.regionprops(label_img, intensity_image=None, cache=True)
+			#print('***', props[0].label, props[0].moments, props[0].moments_central, props[0].moments_hu, props[0].moments_normalized)
+			#print('===', props[0].label, props[0].eccentricity, props[0].minor_axis_length, props[0].major_axis_length)
+			print('Document orientation (statistics) =', math.degrees(props[0].orientation))
+
+			#--------------------
 			# Classic straight-line Hough transform.
-			# Set a precision of 1.0 degree.
-			tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 180)
+			tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 180)  # Set a precision of 1.0 degree.
 			hspace, theta, rho = skimage.transform.hough_line(peak_img, theta=tested_angles)
 
 			origin = np.array((0, peak_img.shape[1]))
-			for idx, (accum, angle, dist) in enumerate(zip(*skimage.transform.hough_line_peaks(hspace, theta, rho, num_peaks=2))):
+			for idx, (accum, angle, dist) in enumerate(zip(*skimage.transform.hough_line_peaks(hspace, theta, rho, min_distance=9, min_angle=10, threshold=None, num_peaks=2))):
+			#for idx, (accum, angle, dist) in enumerate(zip(*skimage.transform.hough_line_peaks(hspace, theta, rho, min_distance=9, min_angle=10, threshold=3, num_peaks=5))):
 				y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
 				y0, y1 = float(y0), float(y1)
 
+				print('#{}: vote = {}.'.format(idx, accum))
+
 				if 0 == idx:
-					cv2.line(rgb, (origin[0], round(y0)), (origin[1], round(y1)), (0, 0, 255), 1, cv2.LINE_AA);
+					cv2.line(rgb, (origin[0], round(y0)), (origin[1], round(y1)), (0, 0, 255), 1, cv2.LINE_AA)
 				else:
-					cv2.line(rgb, (origin[0], round(y0)), (origin[1], round(y1)), (0, 255, 0), 1, cv2.LINE_AA);
-			for pt in peaks:
-				cv2.circle(rgb, tuple(pt[0]), 1, (255, 0, 0), cv2.FILLED, cv2.LINE_AA)
-			cv2.imshow('Line', rgb)
+					cv2.line(rgb, (origin[0], round(y0)), (origin[1], round(y1)), (0, 255, 0), 1, cv2.LINE_AA)
+			cv2.imshow('Result', rgb)
 
 			"""
 			# Generate figure.
@@ -197,105 +211,8 @@ def fft_based_analysis():
 		cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
-# REF [site] >> https://scikit-image.org/docs/dev/auto_examples/edges/plot_line_hough_transform.html
-def hough_line_transform_example():
-	image_filepaths = glob.glob('/work/dataset/text/receipt_sminds/rotated/*.png')
-	for image_filepath in image_filepaths:
-		img = cv2.imread(image_filepath, cv2.IMREAD_GRAYSCALE)
-		if img is None:
-			print('Failed to load an image: {}.'.format(image_filepath))
-			return
-
-		#--------------------
-		# FFT.
-		if True:
-			img_fft = scipy.fftpack.fft2(img)
-		else:
-			# FIXME [error] >> Some artifacts (vertical and horizontal lines) are generated.
-			cols, rows = img.shape[:2]
-			m, n = cv2.getOptimalDFTSize(rows), cv2.getOptimalDFTSize(cols)
-			img_padded = cv2.copyMakeBorder(img, 0, m - rows, 0, n - cols, cv2.BORDER_CONSTANT, 0)
-
-			img_fft = scipy.fftpack.fft2(img_padded)
-			img_fft = img_fft[:cols,:rows]
-		img_fft_mag = np.log10(np.abs(img_fft))
-
-		# Crop the spectrum, if it has an odd number of rows or columns.
-		img_fft_mag = img_fft_mag[:(img_fft_mag.shape[0] & -2),:(img_fft_mag.shape[1] & -2)]
-
-		# Rearrange the quadrants of Fourier image  so that the origin is at the image center.
-		cy, cx = img_fft_mag.shape[0] // 2, img_fft_mag.shape[1] // 2
-		top_left = copy.deepcopy(img_fft_mag[:cy,:cx])
-		top_right = copy.deepcopy(img_fft_mag[:cy,cx:])
-		bottom_left = copy.deepcopy(img_fft_mag[cy:,:cx])
-		bottom_right = copy.deepcopy(img_fft_mag[cy:,cx:])
-		img_fft_mag[:cy,:cx] = bottom_right
-		img_fft_mag[:cy,cx:] = bottom_left
-		img_fft_mag[cy:,:cx] = top_right
-		img_fft_mag[cy:,cx:] = top_left
-		del top_left, top_right, bottom_left, bottom_right
-
-		# Transform the matrix with float values into a viewable image form (float between values 0 and 1).
-		img_fft_mag_normalized = cv2.normalize(img_fft_mag, None, 0, 1, cv2.NORM_MINMAX)
-
-		cv2.imshow('FFT', img_fft_mag_normalized)
-
-		#--------------------
-		# Find peaks.
-		peaks = peak_local_max(img_fft_mag_normalized, min_distance=5, num_peaks=50)
-		tmp = copy.deepcopy(peaks[:,0])
-		peaks[:,0] = peaks[:,1]
-		peaks[:,1] = tmp
-		peaks = np.expand_dims(peaks, axis=1)
-
-		rgb = cv2.cvtColor(np.round(img_fft_mag_normalized * 255).astype(np.uint8), cv2.COLOR_GRAY2BGR)
-		for pt in peaks:
-			cv2.circle(rgb, tuple(pt[0]), 2, (0, 0, 255), cv2.FILLED, cv2.LINE_AA)
-
-		cv2.imshow('Peaks', rgb)
-
-		peak_img = np.zeros(img_fft_mag_normalized.shape, dtype=np.uint8)
-		for pt in peaks:
-			peak_img[pt[0][1], pt[0][0]] = 255
-		image = peak_img
-
-		#--------------------
-		# Classic straight-line Hough transform.
-		# Set a precision of 0.5 degree.
-		tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 360)
-		hspace, theta, d = skimage.transform.hough_line(image, theta=tested_angles)
-
-		# Generate figure.
-		fig, axes = plt.subplots(1, 3, figsize=(15, 6))
-		ax = axes.ravel()
-
-		ax[0].imshow(image, cmap=matplotlib.cm.gray)
-		ax[0].set_title('Input image')
-		ax[0].set_axis_off()
-
-		ax[1].imshow(np.log(1 + hspace),
-					 extent=[np.rad2deg(theta[-1]), np.rad2deg(theta[0]), d[-1], d[0]],
-					 cmap=matplotlib.cm.gray, aspect=1 / 1.5)
-		ax[1].set_title('Hough transform')
-		ax[1].set_xlabel('Angles (degrees)')
-		ax[1].set_ylabel('Distance (pixels)')
-		ax[1].axis('image')
-
-		ax[2].imshow(image, cmap=matplotlib.cm.gray)
-		origin = np.array((0, image.shape[1]))
-		for _, angle, dist in zip(*skimage.transform.hough_line_peaks(hspace, theta, d)):
-			y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
-			ax[2].plot(origin, (y0, y1), '-r')
-		ax[2].set_xlim(origin)
-		ax[2].set_ylim((image.shape[0], 0))
-		ax[2].set_axis_off()
-		ax[2].set_title('Detected lines')
-
-		plt.tight_layout()
-		plt.show()
-
 def main():
-	fft_based_analysis()
+	estimate_orientation_based_on_fft()
 
 #--------------------------------------------------------------------
 
