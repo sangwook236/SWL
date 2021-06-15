6e02265ff095d946cca77f40ee4a9b50a5b4aa49
Sang-Wook Lee <sangwook236@gmail.com>
2021-06-15 12:22:55 +0900

    [Update, Python] A new algorithm and its test were implemented to find n-cycles (n-gons) in a graph with collinear vertices based on the line graph L(G) of an vertex graph G for the original n-cycle algorithm.

--- python/src/swl/math/find_cycles.py
+++ python/src/swl/math/find_cycles.py
@@ -1,20 +1,20 @@
-# Counts cycles of length N (N-cycles) in a connected undirected graph graph.
+# Counts cycles of length N (N-cycles) in a connected undirected graph.
 #	REF [site] >> https://www.geeksforgeeks.org/cycles-of-length-n-in-an-undirected-and-connected-graph/
 def count_cycles(graph, n):
 	# Number of vertices.
 	V = len(graph)
 
-	def DFS(graph, marked, n, vert, start, count):
-		# Mark the vertex vert as visited.
-		marked[vert] = True
+	def DFS(graph, marked, n, curr, start, count):
+		# Mark the vertex curr as visited.
+		marked[curr] = True
 
 		# If the path of length (n - 1) is found.
 		if n == 0:
-			# Mark vert as un-visited to make it usable again.
-			marked[vert] = False
+			# Mark curr as un-visited to make it usable again.
+			marked[curr] = False
 
-			# Check if vertex vert can end with vertex start.
-			if graph[vert][start] == 1:
+			# Check if vertex curr can end with vertex start.
+			if graph[curr][start] == 1:
 				count = count + 1
 				return count
 			else:
@@ -22,15 +22,15 @@ def count_cycles(graph, n):
 
 		# For searching every possible path of length (n - 1).
 		for i in range(V):
-			if marked[i] == False and graph[vert][i] == 1:
+			if marked[i] == False and graph[curr][i] == 1:
 				# DFS for searching path by decreasing length by 1.
 				count = DFS(graph, marked, n - 1, i, start, count)
 
-		# Marking vert as unvisited to make it usable again.
-		marked[vert] = False
+		# Marking curr as unvisited to make it usable again.
+		marked[curr] = False
 		return count
 
-	# All vertex are marked un-visited initially.
+	# All vertices are marked un-visited initially.
 	marked = [False] * V
 
 	# Searching for cycle by using v - n + 1 vertices.
@@ -43,52 +43,117 @@ def count_cycles(graph, n):
 	
 	return count // 2
 
-# Finds cycles of length N (N-cycles) in a connected undirected graph graph.
+# Finds cycles of length N (N-cycles) in a connected undirected graph.
 #	REF [site] >> https://www.geeksforgeeks.org/cycles-of-length-n-in-an-undirected-and-connected-graph/
 def find_cycles(graph, n):
 	# Number of vertices.
 	V = len(graph)
 
-	def DFS(graph, marked, n, vert, start, cycles, curr_path):
-		# Mark the vertex vert as visited.
-		marked[vert] = True
+	def DFS(graph, marked, n, curr, start, curr_path, cycles):
+		# Mark the vertex curr as visited.
+		marked[curr] = True
 
 		# If the path of length (n - 1) is found.
 		if n == 0:
-			# Mark vert as un-visited to make it usable again.
-			marked[vert] = False
+			# Mark curr as un-visited to make it usable again.
+			marked[curr] = False
 
-			# Check if vertex vert can end with vertex start.
-			if graph[vert][start] == 1 and \
+			# Check if vertex curr can end with vertex start.
+			if graph[curr][start] == 1 and \
 				[curr_path[0]] + curr_path[1:][::-1] not in cycles:  # Check if the reversed current path exists in the set of cycles or not.
 				cycles.append(curr_path.copy())
 			return
 
 		# For searching every possible path of length (n - 1).
 		for i in range(V):
-			if marked[i] == False and graph[vert][i] == 1:
+			if marked[i] == False and graph[curr][i] == 1:
 				curr_path.append(i)
 
 				# DFS for searching path by decreasing length by 1.
-				DFS(graph, marked, n - 1, i, start, cycles, curr_path)
+				DFS(graph, marked, n - 1, i, start, curr_path, cycles)
 
 				curr_path.pop()
 
-		# Marking vert as unvisited to make it usable again.
-		marked[vert] = False
+		# Marking curr as unvisited to make it usable again.
+		marked[curr] = False
 
-	# All vertex are marked un-visited initially.
+	# All vertices are marked un-visited initially.
 	marked = [False] * V
 
 	# Searching for cycle by using v - n + 1 vertices.
 	cycles = list()
 	for i in range(V - (n - 1)):
-		DFS(graph, marked, n - 1, i, i, cycles, curr_path=[i])
+		DFS(graph, marked, n - 1, i, i, curr_path=[i], cycles=cycles)
 
 		# i-th vertex is marked as visited and will not be visited again.
 		marked[i] = True
 	
 	return cycles
+
+# Finds cycles of length N (N-cycles) in a connected undirected edge graph.
+#	REF [site] >> https://www.geeksforgeeks.org/cycles-of-length-n-in-an-undirected-and-connected-graph/
+def find_edge_cycles(graph, line_sets, n):
+	# Number of edges.
+	E = len(graph)
+
+	def DFS(graph, line_sets, marked, n, curr, start, edge_path, vertex_path, edge_cycles, vertex_cycles=None):
+		# Mark the edge curr as visited.
+		marked[curr] = True
+
+		# If the path of length (n - 1) is found.
+		if n == 0:
+			# Mark curr as un-visited to make it usable again.
+			marked[curr] = False
+
+			# Check if edge curr can end with edge start.
+			if graph[curr][start] == 1 and \
+				[edge_path[0]] + edge_path[1:][::-1] not in edge_cycles:  # Check if the reversed current path exists in the set of cycles or not.
+				intersection_vertex = line_sets[curr].intersection(line_sets[start])
+				# NOTE [info] >> More than 2 adjacent points can be included in two more sets.
+				#assert len(intersection_vertex) == 1, "{} intersection {} = {}".format(line_sets[curr], line_sets[start], intersection_vertex)
+				intersection_vertex = intersection_vertex.pop()
+
+				if intersection_vertex not in vertex_path:
+					edge_cycles.append(edge_path.copy())
+					vertex_cycles.append(vertex_path + [intersection_vertex])
+			return
+
+		# For searching every possible path of length (n - 1).
+		for i in range(E):
+			if marked[i] == False and graph[curr][i] == 1:
+				intersection_vertex = line_sets[curr].intersection(line_sets[i])
+				# NOTE [info] >> More than 2 adjacent points can be included in two more sets.
+				#assert len(intersection_vertex) == 1, "{} intersection {} = {}".format(line_sets[curr], line_sets[i], intersection_vertex)
+				intersection_vertex = intersection_vertex.pop()
+
+				#if intersection_vertex in vertex_path:
+				if intersection_vertex in vertex_path or any([intersection_vertex in line_sets[eg] for eg in edge_path[:-1]]):
+					continue
+
+				edge_path.append(i)
+				vertex_path.append(intersection_vertex)
+
+				# DFS for searching path by decreasing length by 1.
+				DFS(graph, line_sets, marked, n - 1, i, start, edge_path, vertex_path, edge_cycles, vertex_cycles)
+
+				edge_path.pop()
+				vertex_path.pop()
+
+		# Marking curr as unvisited to make it usable again.
+		marked[curr] = False
+
+	# All edges are marked un-visited initially.
+	marked = [False] * E
+
+	# Searching for cycle by using v - n + 1 edges.
+	edge_cycles, vertex_cycles = list(), list()
+	for i in range(E - (n - 1)):
+		DFS(graph, line_sets, marked, n - 1, i, i, edge_path=[i], vertex_path=[], edge_cycles=edge_cycles, vertex_cycles=vertex_cycles)
+
+		# i-th edge is marked as visited and will not be visited again.
+		marked[i] = True
+	
+	return edge_cycles, vertex_cycles
 
 # Finds cycles of length N (N-cycles) in a connected undirected graph with collinear vertices.
 #	REF [site] >> https://www.geeksforgeeks.org/cycles-of-length-n-in-an-undirected-and-connected-graph/
@@ -96,17 +161,17 @@ def find_cycles_in_graph_with_collinear_vertices(graph, collinear_vertex_sets, n
 	# Number of vertices.
 	V = len(graph)
 
-	def DFS(graph, collinear_vertex_sets, marked, n, vert, start, cycles, curr_path, collinear_flags):
-		# Mark the vertex vert as visited.
-		marked[vert] = True
+	def DFS(graph, collinear_vertex_sets, marked, n, curr, start, curr_path, collinear_flags, cycles):
+		# Mark the vertex curr as visited.
+		marked[curr] = True
 
 		# If the path of length (n - 1) is found.
 		if n == 0:
-			# Mark vert as un-visited to make it usable again.
-			marked[vert] = False
+			# Mark curr as un-visited to make it usable again.
+			marked[curr] = False
 
-			# Check if vertex vert can end with vertex start.
-			if graph[vert][start] == 1:
+			# Check if vertex curr can end with vertex start.
+			if graph[curr][start] == 1:
 				if len(collinear_flags) > n:  # Collinear vertices exist in the cycle.
 					new_cycle = list(vtx for flag, vtx in zip(collinear_flags[1:], curr_path) if not flag) + [curr_path[-1]]  # Removes collinear vertices.
 					if [new_cycle[0]] + new_cycle[1:][::-1] not in cycles:  # Check if the reversed current path exists in the set of cycles or not.
@@ -118,7 +183,7 @@ def find_cycles_in_graph_with_collinear_vertices(graph, collinear_vertex_sets, n
 
 		# For searching every possible path of length (n - 1).
 		for i in range(V):
-			if marked[i] == False and graph[vert][i] == 1:
+			if marked[i] == False and graph[curr][i] == 1:
 				curr_path.append(i)
 				is_collinear = False
 				if len(curr_path) == 3:
@@ -142,21 +207,21 @@ def find_cycles_in_graph_with_collinear_vertices(graph, collinear_vertex_sets, n
 				collinear_flags.append(is_collinear)
 
 				# DFS for searching path by decreasing length by 1.
-				DFS(graph, collinear_vertex_sets, marked, n if is_collinear else n - 1, i, start, cycles, curr_path, collinear_flags)
+				DFS(graph, collinear_vertex_sets, marked, n if is_collinear else n - 1, i, start, curr_path, collinear_flags, cycles)
 
 				curr_path.pop()
 				collinear_flags.pop()
 
-		# Marking vert as unvisited to make it usable again.
-		marked[vert] = False
+		# Marking curr as unvisited to make it usable again.
+		marked[curr] = False
 
-	# All vertex are marked un-visited initially.
+	# All vertices are marked un-visited initially.
 	marked = [False] * V
 
 	# Searching for cycle by using v - n + 1 vertices.
 	cycles = list()
 	for i in range(V - (n - 1)):
-		DFS(graph, collinear_vertex_sets, marked, n - 1, i, i, cycles, curr_path=[i], collinear_flags=[False])
+		DFS(graph, collinear_vertex_sets, marked, n - 1, i, i, curr_path=[i], collinear_flags=[False], cycles=cycles)
 
 		# i-th vertex is marked as visited and will not be visited again.
 		marked[i] = True
--- python/test/math/find_cycles_test.py
+++ python/test/math/find_cycles_test.py
@@ -2,11 +2,11 @@
 # -*- coding: UTF-8 -*-
 
 import sys
-sys.path.append('../../src')
-sys.path.append('./src')
+sys.path.append("../../src")
+sys.path.append("./src")
 
 import time
-from swl.math.find_cycles import count_cycles, find_cycles, find_cycles_in_graph_with_collinear_vertices
+from swl.math.find_cycles import count_cycles, find_cycles, find_edge_cycles, find_cycles_in_graph_with_collinear_vertices
 
 def main():
 	# NOTE [info] >> Find n-cycles in a graph ==> Polygon and quadrilateral detection.
@@ -98,28 +98,70 @@ def main():
 	n = 4
 	#n = 5
 
-	print('Start counting n-cycles in a graph...')
+	print("Start counting {}-cycles in a graph...".format(n))
 	start_time = time.time()
 	num_cycles = count_cycles(graph, n)
-	print('End counting n-cycles in a graph: {} secs.'.format(time.time() - start_time))
+	print("End counting {}-cycles in a graph: {} secs.".format(n, time.time() - start_time))
 	print("#cycles of length {} = {}.".format(n, num_cycles))
 
-	print('Start finding n-cycles in a graph...')
+	print("Start finding {}-cycles in a graph...".format(n))
 	start_time = time.time()
 	cycles = find_cycles(graph, n)
-	print('End finding n-cycles in a graph: {} secs.'.format(time.time() - start_time))
+	print("End finding {}-cycles in a graph: {} secs.".format(n, time.time() - start_time))
 	print("Cycles of length {} = {}.".format(n, cycles))
 
 	assert num_cycles == len(cycles)
 
+	# Find n-cycles in a graph with collinear vertices. 
+	#	Use the line graph L(G) of the original graph G.
 	if collinear_vertex_sets:
-		print('Start finding n-cycles in a graph with collinear vertices...')
+		# Constructs a set of line segments.
+		line_segment_sets = collinear_vertex_sets.copy()
+		for ith, row in enumerate(graph):
+			#for jth, flag in enumerate(row):
+			#	if flag:
+			for jth in range(ith):
+				if row[jth]:
+					new_ls = {ith, jth}
+					is_new = True
+					for line_segment_set in line_segment_sets:
+						if new_ls.issubset(line_segment_set):
+							is_new = False
+							break
+					if is_new: line_segment_sets.append(new_ls)
+
+		# Number of line segments.
+		LS = len(line_segment_sets)
+
+		# Constructs an adjacency matrix of the line graph L(G) of the original graph G.
+		#	REF [site] >> https://en.wikipedia.org/wiki/Line_graph
+		line_graph = [[0] * LS for _ in range(LS)]
+		for ith, ith_ls in enumerate(line_segment_sets):
+			for jth, jth_ls in enumerate(line_segment_sets):
+				if ith != jth and ith_ls.intersection(jth_ls):
+					line_graph[ith][jth] = line_graph[jth][ith] = 1
+
+		print("Start finding {}-cycles in the line graph of a graph...".format(n))
 		start_time = time.time()
-		cycles = find_cycles_in_graph_with_collinear_vertices(graph, collinear_vertex_sets, n)
-		print('End finding n-cycles in a graph with collinear vertices: {} secs.'.format(time.time() - start_time))
-		print("Cycles of length {} = {}.".format(n, cycles))
+		edge_cycles, vertex_cycles= find_edge_cycles(line_graph, line_segment_sets, n)
+		print("End finding {}-cycles in the line graph of a graph: {} secs.".format(n, time.time() - start_time))
+		print("Edge cycles of length {} = {}.".format(n, edge_cycles))
+		print("Vertex cycles of length {} = {}.".format(n, vertex_cycles))
+
+	# FIXME [fix] >> Incorrect results.
+	# Find n-cycles in a graph with collinear vertices.
+	if False:
+		if collinear_vertex_sets:
+			print("Start finding {}-cycles in a graph with collinear vertices...".format(n))
+			start_time = time.time()
+			cycles = find_cycles_in_graph_with_collinear_vertices(graph, collinear_vertex_sets, n)
+			print("End finding {}-cycles in a graph with collinear vertices: {} secs.".format(n, time.time() - start_time))
+			print("Cycles of length {} = {}.".format(n, cycles))
+
+	# NOTE [info] >> Apply some constraints to filter valid polygons (n-gons, n-cycles).
+	#	REF [function] >> detect_quadrilaterals_by_lines() in ${DataAnalysis_HOME}/app/id/detect_region.py
 
 #--------------------------------------------------------------------
 
-if '__main__' == __name__:
+if "__main__" == __name__:
 	main()
